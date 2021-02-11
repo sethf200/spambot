3cedd3f38e905e97da6d7114da33c1aeef5419ba
Seth Foster <sethfoster20@protonmail.com>
2021-02-11 03:38:26 -0500

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -2,6 +2,5 @@ import pyautogui, time
 time.sleep(10)
 f = open("beemovie", "r")
 for word in f:
-	pyautogui.press("/")
-	pyautogui.typewrite(word, interval=0.05)
+	pyautogui.typewrite(word, interval=0.02)
 	pyautogui.press("enter")	
